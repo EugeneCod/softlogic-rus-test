8005ca96788314d12380b1c36e18887e4faf2481
EugeneKGen <eugene_korobkin@mail.ru>
2025-03-13 12:00:51 +0300

    feat : add cards

--- src/components/card-list/card-list.tsx
+++ src/components/card-list/card-list.tsx
@@ -1,4 +1,111 @@
-export const CardList = (props: PropsWithClassName) => {
+import './card-list.scss';
+import { Card } from '..';
+import Image1 from '@/assets/images/card-image__1.jfif';
+import Image2 from '@/assets/images/card-image__2.jfif';
+import type { CardProps } from '../card/card';
+
+const cards: CardProps[] = [
+  {
+    title: 'Пролет №1',
+    fileType: 'Фото',
+    date: '23.01.2023',
+    tags: ['forest', 'frames'],
+    imageSrc: Image1,
+    numberOfFiles: 12,
+  },
+  {
+    title: 'Пролет №2',
+    fileType: 'Видео',
+    date: '23.01.2023',
+    tags: ['forest', 'spans'],
+    imageSrc: Image2,
+    numberOfFiles: 2,
+  },
+  {
+    title: 'Пролет №3',
+    fileType: 'Видео',
+    date: '23.01.2023',
+    tags: ['forest', 'spans'],
+    imageSrc: Image2,
+    numberOfFiles: 3,
+  },
+  {
+    title: 'Пролет №4',
+    fileType: 'Фото',
+    date: '23.01.2023',
+    tags: ['forest', 'frames'],
+    imageSrc: Image1,
+    numberOfFiles: 7,
+  },
+  {
+    title: 'Пролет №5',
+    fileType: 'Фото',
+    date: '23.01.2023',
+    tags: ['forest', 'frames'],
+    imageSrc: Image1,
+    numberOfFiles: 8,
+  },
+  {
+    title: 'Пролет №6',
+    fileType: 'Видео',
+    date: '23.01.2023',
+    tags: ['forest', 'spans'],
+    imageSrc: Image2,
+    numberOfFiles: 22,
+  },
+  {
+    title: 'Пролет №7',
+    fileType: 'Фото',
+    date: '23.01.2023',
+    tags: ['forest', 'frames'],
+    imageSrc: Image1,
+    numberOfFiles: 45,
+  },
+  {
+    title: 'Пролет №8',
+    fileType: 'Фото',
+    date: '23.01.2023',
+    tags: ['forest', 'frames'],
+    imageSrc: Image1,
+    numberOfFiles: 1,
+  },
+  {
+    title: 'Пролет №9',
+    fileType: 'Видео',
+    date: '23.01.2023',
+    tags: ['forest', 'spans'],
+    imageSrc: Image2,
+    numberOfFiles: 9,
+  },
+  {
+    title: 'Пролет №10',
+    fileType: 'Фото',
+    date: '23.01.2023',
+    tags: ['forest', 'frames'],
+    imageSrc: Image1,
+    numberOfFiles: 17,
+  },
+];
+
+interface CardListProps extends PropsWithClassName {
+  cards: CardProps;
+}
+
+export const CardList = (props: CardListProps) => {
   const { className } = props;
-  return <div className={className}>CardList</div>;
+  return (
+    <ul className={`card-list ${className}`}>
+      {cards.map((card, index) => (
+        <Card
+          key={index}
+          title={card.title}
+          fileType={card.fileType}
+          date={card.date}
+          tags={card.tags}
+          imageSrc={card.imageSrc}
+          numberOfFiles={card.numberOfFiles}
+        />
+      ))}
+    </ul>
+  );
 };
--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,7 +1,144 @@
-import React from 'react'
+import './card.scss';
+import PhotoFilmIcon from '@/assets/images/icons/photo-film.svg';
+import GearIcon from '@/assets/images/icons/gear.svg';
+import ChartNetworkIcon from '@/assets/images/icons/chart-network.svg';
+import EyeIcon from '@/assets/images/icons/eye.svg';
+import FileImageIcon from '@/assets/images/icons/file-image.svg';
+import FileVideoIcon from '@/assets/images/icons/file-video.svg';
+import MobilePhoneIcon from '@/assets/images/icons/mobile-phone.svg';
+import SeatBeltIcon from '@/assets/images/icons/seat-belt.svg';
+import SleepIcon from '@/assets/images/icons/sleep.svg';
+import TrashIcon from '@/assets/images/icons/trash.svg';
+import { IconButton } from '@/ui';
 
-export const Card = () => {
-  return (
-    <div>Card</div>
-  )
+type Tags = 'forest' | 'frames' | 'spans';
+
+export interface CardProps {
+  title: string;
+  fileType: 'Фото' | 'Видео';
+  date: string;
+  tags: Tags[];
+  imageSrc: string;
+  numberOfFiles: number;
 }
+
+const tagsData: Record<Tags, { text: string; style: string }> = {
+  forest: {
+    text: 'Лес',
+    style: 'green',
+  },
+  frames: {
+    text: 'Кадры',
+    style: 'orange',
+  },
+  spans: {
+    text: 'Полеты',
+    style: 'red',
+  },
+};
+
+const neuronetList = [
+  { iconSrc: TrashIcon },
+  { iconSrc: SeatBeltIcon },
+  { iconSrc: MobilePhoneIcon },
+  { iconSrc: SleepIcon },
+  { iconSrc: TrashIcon },
+  { iconSrc: SeatBeltIcon },
+];
+
+export const Card = (props: CardProps) => {
+  const { title, fileType, date, tags, imageSrc, numberOfFiles } = props;
+
+  const fileIconSrc = fileType === 'Фото' ? FileImageIcon : FileVideoIcon;
+
+  return (
+    <li className="card">
+      <div className="card__top">
+        <div className="card__title-block">
+          <img className="card__title-image" src={PhotoFilmIcon} alt="Фото/видео" />
+          <span className="card__title">{title}</span>
+        </div>
+        <img className="card__image" src={imageSrc} alt={imageSrc} />
+      </div>
+
+      <div className="card__bottom">
+        <div className="card__row">
+          <div className="card__props-left-col">
+            <span className="card__props-key">ID</span>
+          </div>
+          <div className="card__props-right-col">
+            <span className="card__props-value">663с7…a2d1</span>
+            <button className="card__props-copy-btn" type="button" />
+          </div>
+        </div>
+        <div className="card__row">
+          <div className="card__props-left-col">
+            <span className="card__props-key">Тип файлов</span>
+            <img className="card__file-icon" src={fileIconSrc} alt={fileType} />
+            <span className="card__props-value">{fileType}</span>
+          </div>
+          <div className="card__props-right-col">
+            <span className="card__props-key">Кол-во файлов</span>
+            <span className="card__props-value">{numberOfFiles}</span>
+          </div>
+        </div>
+        <div className="card__row">
+          <div className="card__props-left-col">
+            <span className="card__props-key">Дата добавления</span>
+          </div>
+          <div className="card__props-right-col">
+            <span className="card__props-value">{date}</span>
+          </div>
+        </div>
+        <div className="card__row">
+          <div className="card__props-left-col">
+            <span className="card__props-key">Теги</span>
+          </div>
+          <div className="card__props-right-col">
+            <ul className="card__tags-list">
+              {tags.map((tag, index) => (
+                <li
+                  className={`card__tags-list-item card__tags-list-item_${tagsData[tag].style}`}
+                  key={index}>
+                  #{tagsData[tag].text}
+                </li>
+              ))}
+            </ul>
+            <button className="card__unwrap-btn" type="button" />
+          </div>
+        </div>
+        <div className="card__row">
+          <div className="card__props-left-col">
+            <span className="card__props-key">Нейросети</span>
+          </div>
+          <div className="card__props-right-col">
+            <ul className="card__neuronet-list">
+              {neuronetList.map(({ iconSrc }, index) => (
+                <li className={`card__neuronet-list-item`} key={index}>
+                  <img src={iconSrc} alt={iconSrc} />
+                </li>
+              ))}
+            </ul>
+            <button className="card__unwrap-btn" type="button" />
+          </div>
+        </div>
+        <div className="card__row">
+          <div className="card__props-left-col">
+            <span className="card__props-key">Количество</span>
+            <img
+              className="card__chart-network-icon"
+              src={ChartNetworkIcon}
+              alt={ChartNetworkIcon}
+            />
+            <span className="card__props-value card__props-value_blue">12</span>
+            <IconButton iconSrc={EyeIcon} size={'small'} />
+          </div>
+          <div className="card__props-right-col">
+            <span className="card__props-key">Управление</span>
+            <IconButton iconSrc={GearIcon} size={'small'} />
+          </div>
+        </div>
+      </div>
+    </li>
+  );
+};
